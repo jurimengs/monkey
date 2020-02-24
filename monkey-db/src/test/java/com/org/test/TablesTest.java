39b78a644b1bb6c51ebaeb497b2c3361e68f25e8
zhouman <jurimengs@163.com>
2020-02-24 14:57:54 +0800

    差一个 入库的实现

--- monkey-db/src/test/java/com/org/test/TablesTest.java
+++ monkey-db/src/test/java/com/org/test/TablesTest.java
@@ -1,7 +1,15 @@
 package com.org.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.monkey.db.core.executor.Executor;
+import org.monkey.db.core.executor.MysqlExecutor;
 import org.monkey.db.core.store.Store;
 import org.monkey.db.core.store.Tabels;
+
+import com.monkey.base.KeyValue;
+import com.monkey.exceptions.DataException;
 
 public class TablesTest {
     public static void main(String[] args) {
@@ -9,14 +17,40 @@ public class TablesTest {
         new Thread() {
             @Override
             public void run() {
-                Tabels.getInstance();
+                Executor executor = new MysqlExecutor();
+                Store store = Tabels.getInstance().getStore(tableName, executor);
+                String keyFieldName = "id";
+                try {
+                    List<KeyValue> datas = new ArrayList<>();
+                    KeyValue keyValue = new KeyValue();
+                    keyValue.setKey(keyFieldName);
+                    keyValue.setValue("333aaa");
+                    
+                    datas.add(keyValue);
+                    store.add(keyFieldName, datas );
+                } catch (DataException e) {
+                    e.printStackTrace();
+                }
             }
         }.start();
         
         new Thread() {
             @Override
             public void run() {
-                Tabels.getInstance();
+                Executor executor = new MysqlExecutor();
+                Store store = Tabels.getInstance().getStore(tableName, executor);
+                String keyFieldName = "id";
+                try {
+                    List<KeyValue> datas = new ArrayList<>();
+                    KeyValue keyValue = new KeyValue();
+                    keyValue.setKey(keyFieldName);
+                    keyValue.setValue("abcdedf");
+                    
+                    datas.add(keyValue);
+                    store.add(keyFieldName, datas );
+                } catch (DataException e) {
+                    e.printStackTrace();
+                }
             }
         }.start();
         
